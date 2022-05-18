866c4a90cb0dc7b8f1236e5eaf4bf9dce13bca29
Rolando Mamani Salas <rolando.mamani@fundacion-jala.org>
2022-05-18 06:12:20 -0400

    Se configuro el POJO a nivel del properties

--- src/main/java/com/fundamentos/spring/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/spring/fundamentos/FundamentosApplication.java
@@ -5,6 +5,7 @@ import com.fundamentos.spring.fundamentos.bean.MyBeanWIthDependency;
 import com.fundamentos.spring.fundamentos.bean.MyBeanWIthProperties;
 import com.fundamentos.spring.fundamentos.bean.Mybean;
 import com.fundamentos.spring.fundamentos.component.ComponentDependency;
+import com.fundamentos.spring.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -19,12 +20,14 @@ public class FundamentosApplication implements CommandLineRunner {
 	private MyBeanWIthDependency myBeanWIthDependency;
 
 	private MyBeanWIthProperties myBeanWIthProperties;
+	private UserPojo userPojo;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, Mybean myBean, MyBeanWIthDependency myBeanWIthDependency, MyBeanWIthProperties myBeanWIthProperties){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, Mybean myBean, MyBeanWIthDependency myBeanWIthDependency, MyBeanWIthProperties myBeanWIthProperties, UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWIthDependency = myBeanWIthDependency;
 		this.myBeanWIthProperties = myBeanWIthProperties;
+		this.userPojo = userPojo;
 	}
 
 
@@ -38,5 +41,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBean.print();
 		myBeanWIthDependency.printWithDependency();
 		System.out.println(myBeanWIthProperties.function());
+		System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
 	}
 }
--- src/main/java/com/fundamentos/spring/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/spring/fundamentos/configuration/GeneralConfiguration.java
@@ -2,11 +2,15 @@ package com.fundamentos.spring.fundamentos.configuration;
 
 import com.fundamentos.spring.fundamentos.bean.MyBeanWIthProperties;
 import com.fundamentos.spring.fundamentos.bean.MyBeanWIthPropertiesImplement;
+import com.fundamentos.spring.fundamentos.pojo.UserPojo;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
